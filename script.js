6096451b6b3e4d59861c8232c56e281634442dcf
Ivan Knysh <mr.rauber.ik@gmail.com>
2023-12-22 16:11:15 +0200

    Init project

---
+++ script.js
@@ -0,0 +1,101 @@
+const kitchenProducts = [
+	{
+		type: "grater",
+		price: 10,
+	},
+	{
+		type: "pastry-bag",
+		price: 25,
+	},
+	{
+		type: "scale",
+		price: 5,
+	},
+	{
+		type: "whisk",
+		price: 15,
+	},
+];
+
+const devicesProducts = [
+	{
+		type: "desktop",
+		price: [100, 1000],
+	},
+	{
+		type: "laptop",
+		price: [50, 1500],
+	},
+	{
+		type: "smartphone",
+		price: [80, 2000],
+	},
+	{
+		type: "tablet",
+		price: [20, 1300],
+	},
+];
+
+const cosmeticsProducts = [
+	{
+		type: "blush",
+		price: 100,
+	},
+	{
+		type: "eyeshadow",
+		price: 50,
+	},
+	{
+		type: "lipstick",
+		price: 80,
+	},
+	{
+		type: "nail-polish",
+		price: 200,
+	},
+	{
+		type: "perfume",
+		price: 300,
+	},
+];
+
+const Kitchen = { category: "kitchen" };
+const Devices = { category: "devices" };
+const Cosmetics = { category: "cosmetics" };
+
+kitchenProducts.forEach((product) => Object.setPrototypeOf(product, Kitchen));
+devicesProducts.forEach((product) => Object.setPrototypeOf(product, Devices));
+cosmeticsProducts.forEach((product) =>
+	Object.setPrototypeOf(product, Cosmetics)
+);
+
+kitchenProducts.forEach((product) => (product.category = "kitchen"));
+devicesProducts.forEach((product) => (product.category = "devices"));
+cosmeticsProducts.forEach((product) => (product.category = "cosmetics"));
+
+const randerProduct = (arr) => {
+	const categoryName = arr[0].category;
+
+	document.querySelector(".wrapper").insertAdjacentHTML(
+		"beforeend",
+		`<div class="category-item" id="${categoryName}">
+			<h2>Category: ${categoryName}</h2>
+			<div class="flx"></div>
+		</div>`
+	);
+
+	arr.forEach((item) => {
+		document.querySelector(`.wrapper #${categoryName} .flx`).insertAdjacentHTML(
+			"beforeend",
+			`<div class="product-item">
+					<img src="./images/${categoryName}/${item.type}.svg" alt="${item.type}" />
+					<div class="info">Name: <span>${item.type}</span></div>
+					<div class="info">Price: <span>$${item.price}</span></div>
+				</div>`
+		);
+	});
+};
+
+randerProduct(kitchenProducts);
+randerProduct(devicesProducts);
+randerProduct(cosmeticsProducts);
